e7aef725d881fb6d1410b226b973444d886b494b
hzk2021 <hezhenkai1999@gmail.com>
2023-08-12 22:14:09 +0800

    Update README

--- DrawBot/Win32.cs
+++ DrawBot/Win32.cs
@@ -12,29 +12,23 @@ namespace DrawBot
 {
     static class Win32
     {
-        // configure mouse window events
         public const int HT_CAPTION = 0x2;
         public const int WM_NCLBUTTONDOWN = 0xA1;
 
-        // grab dlls for mousedown
         [DllImport("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
         [DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
 
-        // grab dlls for mouse controls
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
-        // mouse inputs
         private const int MOUSEEVENTF_LEFTDOWN = 0x02;
         private const int MOUSEEVENTF_LEFTUP = 0x04;
 
-        // key detection script
         [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
-        // 0x01 = leftmouse
-        // 0x1B = esc
+
         public static bool keyDetect(string key)
         {
             int keyInt = 0x00;
